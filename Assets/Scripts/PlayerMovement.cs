305fcbc9ae3f04c0b7abdd24492f4376600b21b4
mikeb <mikebeelz214@gmail.com>
2026-06-03 15:59:32 -0400

    Extemely Simple C# Script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    void start()
+    {
+        Debug.Log("Game Started");
+    }
+
+    void Update()
+    {
+        Debug.Log("Game Running");
+    }
+}
