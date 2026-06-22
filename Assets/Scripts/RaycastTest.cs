af16e7865f0e36be5939b14db29c239f54434dc3
mikeb <mikebeelz214@gmail.com>
2026-06-22 19:55:25 -0400

    Added Push Logic

--- Assets/Scripts/RaycastTest.cs
+++ Assets/Scripts/RaycastTest.cs
@@ -14,32 +14,50 @@ public class RaycastTest : MonoBehaviour
         // If left mouse is clicked
         if (Input.GetMouseButtonDown(0))
         {
-            // Create a RaycastHit object, which will contain info on clicked objects
-            RaycastHit hit;
+            processMouse(0);
+        }
 
-            // If something was hit, print its name in the console
-            if (Physics.Raycast(transform.position, transform.forward, out hit, 100f))
+        else if (Input.GetMouseButtonDown(1))
+        {
+            processMouse(1);
+        }     
+    }
+
+    void processMouse (int buttonPressed)
+    {
+        // Create a RaycastHit object, which will contain info on clicked objects
+        RaycastHit hit;
+
+        // If something was hit, print its name in the console
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 100f))
+        {
+            Debug.Log("Hit: " + hit.collider.name);
+
+            // Does the hit object have a MagneticObject Component?
+            if (hit.collider.GetComponent<MagneticObject>())
             {
-                Debug.Log("Hit: " + hit.collider.name);
+                Debug.Log("This object is magnetic");
 
-                // Does the hit object have a MagneticObject Component?
-                if (hit.collider.GetComponent<MagneticObject>())
+                // Compute direction from cube towards player
+                Vector3 start = hit.collider.transform.position;
+                Vector3 target = transform.position;
+                Vector3 heading = target - start;
+
+                // Normalize the vector
+                Vector3 dir = heading.normalized;
+                Debug.Log("Dir: " + dir);
+
+                // Add force to the hit object
+                if (buttonPressed == 0)
                 {
-                    Debug.Log("This object is magnetic");
-
-                    // Compute direction from cube towards player
-                    Vector3 start = hit.collider.transform.position;
-                    Vector3 target = transform.position;
-                    Vector3 heading = target - start;
-
-                    // Normalize the vector
-                    Vector3 dir = heading.normalized;
-                    Debug.Log("Dir: " + dir);
-
-                    // Add force to the hit object
                     hit.collider.attachedRigidbody.AddForce(dir * 500);
                 }
+
+                else 
+                {
+                    hit.collider.attachedRigidbody.AddForce(-dir * 500);
+                }
             }
-        }     
+        }
     }
 }
